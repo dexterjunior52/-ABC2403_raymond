9cd7853ff5a2ec9ce1779f19dd25aa3801ff647e
dexterjunior52 <richard.raymond52@outlook.fr>
2024-04-25 09:06:55 +0200

    helloyou
    prenom

---
+++ Algorithmes/helloYou/src/helloYou/App.java
@@ -0,0 +1,31 @@
+package helloYou;
+
+import java.util.Scanner;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String prenom ; // déclaration d'une variable " prenom" de type " chaine de caracteres "
+		
+		Scanner sc;// Déclaration d'une variable de type Scanner (penser à ajouter la ligne d'import ( cf ligne 3) )
+		
+		sc = new Scanner ( System.in ); // Initianisation d'un nouveau Scanner
+		
+		System.out.println ( "Bonjour,veuillez saisir votre prenom "); // Affichage
+		
+		prenom = sc.nextline (); // Demande à l'utilisateur une saisie .
+		
+		// lorsque l'utilisateur validera avec la touche "Entrée " ,la saisie est récupérée dans la variable a gauche du signe "="
+		
+		System.out println ;(" Bonjour "+ prenom ) ; //Affichage 
+		
+		sc.close ();//fERMETURE DU SCANNER 5obligatoirement )
+		
+		
+
+	}
+
+}
