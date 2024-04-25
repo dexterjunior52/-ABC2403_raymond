0381f6a7c77373fccc8eba10a2fac842cd575581
dexterjunior52 <richard.raymond52@outlook.fr>
2024-04-25 12:01:25 +0200

    CALCUL DE  NOMBRES
    OK

--- Algorithmes/moyenne de nombres/src/moyennedenombre.java
+++ Algorithmes/moyenne de nombres/src/moyennedenombre.java
@@ -1,9 +1,40 @@
 
-public class moyennedenombre {
+
+package  moyenne2nombres;
+
+ import java.util.Scanner;
+
+  Import java.util.Scanner;// import du composant scanner
+  
+   public class App ()
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+		int nombre1;
+	
+		int nombre2;
+		
+		float moyenne;
+		
+		Scanner sc;
+		
+		sc =new Scanner(System.in);
+		
+		System.out.prinln("veuillez saisir un premier nombre");
+		
+		nombre1 = sc.nextlnt();
+		
+		System.out.print("veuillez saisir un second nombre :")
+		 /
+		nombre2 =sc.nextlnt();
+		
+		moyenne = (nombre1 +nombre2)/2
+				
+		System.out.println(" la moyenne de "+ nombre1+" et"+ nombre2 +" est :"+moyenne);
+		
+		sc.close();
 
 	}
 
-}
+ 
