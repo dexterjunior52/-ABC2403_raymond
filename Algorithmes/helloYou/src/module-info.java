f6ad2132ea5a7e00d6cc69cc2901c04459c98418
dexterjunior52 <richard.raymond52@outlook.fr>
2024-04-30 09:20:04 +0200

    helloyou
    yes

--- Algorithmes/helloYou/src/module-info.java
+++ Algorithmes/helloYou/src/module-info.java
@@ -1,2 +1,23 @@
-module helloYou {
-}
+//Exercice 2.1 : Positif ou négatif ?
+
+//Écrire un algorithme qui demande à l’utilisateur de saisir 1 nombre entier et l’informe ensuite si le nombre est positif ou négatif.
+
+//Pour information, le nombre 0 est considéré positif.
+
+  VARIABLES ;
+
+     nombreEntier est un ENTIER;
+
+   TRAITEMENT
+
+     ECRIRE "sAISISSEZ UN NOMBRE ENTIER ;
+
+     LIRE nombreEntier;
+
+   SI nombreEntier  >= 0 alors
+    		 
+     ECRIRE "ce nom est positif "
+  
+   SINON ALORS
+     
+     ECRIRE "ce nombre est négatif "
